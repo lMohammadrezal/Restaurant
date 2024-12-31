1b6cbde5d5b38c86c1a91ebce4a06f95c3956fb8
Mohammadreza <mohammadreza.mohammadi8313@gmail.com>
2024-12-31 17:21:42 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -10,26 +10,30 @@ import Team from './pages/Team/Team';
 import Testimonial from './pages/Testimonial/Testimonial';
 import Contact from './pages/Contact/Contact';
 
+
+
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/Restaurant">
         <Layout>
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/services' element={<Services/>}/>
-            <Route path='/menu' element={<Menu/>}/>
-            <Route path='/booking' element={<Booking/>}/>
-            <Route path='/team' element={<Team/>}/>
-            <Route path='/testimonial' element={<Testimonial/>}/>
-            <Route path='/contact' element={<Contact/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </Layout>
       </BrowserRouter>
-
     </div>
   );
-}
+};
 
 export default App;
+
+
+
